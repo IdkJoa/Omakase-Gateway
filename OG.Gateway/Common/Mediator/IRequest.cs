6dd6557b37c415d0094bb3d3b94c0540ea530553
IdkJoa <joamelonunez@gmail.com>
2026-07-01 16:04:02 -0400

    chore: removing some unnecesary comments.

--- OG.Gateway/Common/Mediator/IRequest.cs
+++ OG.Gateway/Common/Mediator/IRequest.cs
@@ -1,6 +1,3 @@
 namespace Application.Common.Mediator;
 
-/// <summary>
-/// Marca un objeto como request del mediador que produce una respuesta de tipo <typeparamref name="TResponse"/>.
-/// </summary>
 public interface IRequest<TResponse> { }
--- OG.Gateway/Common/Mediator/IRequestHandler.cs
+++ OG.Gateway/Common/Mediator/IRequestHandler.cs
@@ -1,8 +1,5 @@
 namespace Application.Common.Mediator;
 
-/// <summary>
-/// Contrato que todo handler debe implementar para procesar un request del mediador.
-/// </summary>
 public interface IRequestHandler<TRequest, TResponse>
     where TRequest : IRequest<TResponse>
 {
